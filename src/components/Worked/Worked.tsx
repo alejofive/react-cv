75091a6e5c0dca3fbe4568ed0775f7efab7a47ac
Alejandro Garcia <alejofive555@gmail.com>
2023-05-13 09:29:17 -0500

    detalles listos

--- src/components/Worked/Worked.tsx
+++ src/components/Worked/Worked.tsx
@@ -1,15 +1,25 @@
 import "./style.css";
 
+const workedImages = [
+  {
+    img1: "./img/amazon.png",
+    img2: "./img/apple.png",
+    img3: "./img/facebook.png",
+  },
+];
+
 const Worked = () => {
   return (
     <>
       <section className="box-worked">
         <h1>Worked with</h1>
-        <div className="worked-img">
-          <img src="./img/amazon.png" alt="" />
-          <img src="./img/apple.png" alt="" />
-          <img src="./img/facebook.png" alt="" />
-        </div>
+        {workedImages.map((workedImage) => (
+          <div className="worked-img">
+            <img src={workedImage.img1} alt="" />
+            <img src={workedImage.img2} alt="" />
+            <img src={workedImage.img3} alt="" />
+          </div>
+        ))}
       </section>
     </>
   );
